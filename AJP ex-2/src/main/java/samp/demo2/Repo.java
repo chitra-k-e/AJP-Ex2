92d4bb146a104dc5c7f1d48f1f3d784b04f1e083
chitra-k-e <148533367+chitra-k-e@users.noreply.github.com>
2023-10-27 19:10:39 +0530

    Add files via upload

---
+++ AJP ex-2/src/main/java/samp/demo2/Repo.java
@@ -0,0 +1,7 @@
+package samp.demo2;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Repo extends CrudRepository<User,Integer>{
+    
+}
---
+++ AJP ex-2/src/main/java/samp/demo2/User.java
@@ -0,0 +1,26 @@
+package samp.demo2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Data")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String dob;
+
+}
